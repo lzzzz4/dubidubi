8302d05c6713ffb5dd953a05325ff4a7eddace3a
16224 <16224@192.168.9.105>
2018-02-05 20:53:35 +0800

    更新了python爬取新闻的代码 新增了定时爬取的测试demo

--- src/main/java/cn/dubidubi/controller/WxController.java
+++ src/main/java/cn/dubidubi/controller/WxController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/cn/dubidubi/util/news/RunPy.java
+++ src/main/java/cn/dubidubi/util/news/RunPy.java
@@ -2,18 +2,34 @@ package cn.dubidubi.util.news;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
 
 public class RunPy {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		URL path = RunPy.class.getClassLoader().getResource("py/requestsStudy.py");
+		URL path = RunPy.class.getClassLoader().getResource("py/newsSpider.py");
 		String filePath = StringUtils.substring(path.getPath(), 1);
 		System.out.println(filePath);
-		String[] strings = { "python", filePath };
+		// String WEBPath =
+		// request.getSession().getServletContext().getRealPath("/");
+		String WEBPath = "H:/news/";
+		System.out.println(WEBPath);
+		String[] strings = { "python", filePath, WEBPath };
 		Process process = Runtime.getRuntime().exec(strings);
 		System.out.println("开始执行");
+		String pystr = new String(IOUtils.toString(process.getInputStream(), Charset.forName("utf-8")));
+		System.out.println(pystr);
 		process.waitFor();
 		System.out.println("结束执行");
+		LoggerFactory.getLogger(RunPy.class).info("python爬取澎湃执行完毕");
 	}
+
 }
